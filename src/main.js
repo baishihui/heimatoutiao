2b9d4d882b124f20ba7548e5a3f31be8cc950713
baishihui <baishihui_97@163.com>
2019-12-19 19:56:18 +0800

    黑马头条PC-统一全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import ElementUI from 'element-ui' // 引入UI
 import './styles/index.less' // 引入初始化样式
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import axios from 'axios'
+import Component from './components' // 引入全局组件
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
+Vue.use(Component) // 全局组件注册
 Vue.prototype.$axios = axios // 赋值给全局对象
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值黑马头条的默认地址
 new Vue({
