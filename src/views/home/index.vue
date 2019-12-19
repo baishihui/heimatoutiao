d1bfbd0ffe0559483278544a76d3a215ac9f5511
baishihui <baishihui_97@163.com>
2019-12-19 16:43:07 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,19 @@
 <template>
 <div>
-    我是主页
+    <!-- 放置一个外层容器 -->
+    <el-container>
+        <!-- 左右布局 -->
+        <el-aside>侧边栏</el-aside>
+          <el-container>
+              <!-- 上下布局 -->
+
+              <el-header>顶部</el-header>
+
+              <!-- 主要区域 -->
+              <el-main>主要区域</el-main>
+
+          </el-container>
+    </el-container>
 </div>
 </template>
 
