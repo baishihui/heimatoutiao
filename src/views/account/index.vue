05e204d382f97ced0dd981161f74e3072002b08e
baishihui <baishihui_97@163.com>
2019-12-27 19:50:31 +0800

    黑马头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,22 +5,22 @@
     </bread-crumb>
     <!-- 放置上传组件 -->
     <el-upload :http-request="uploadImg" class='head-upload' action="" :show-file-list="false">
-        <img src="../../assets/img/header-logo.jpg" alt="">
+        <img :src="formData.photo ? formData.photo :defaultImg " alt="">
 
     </el-upload>
     <!-- 放置表单组件 -->
-    <el-form style="margin-left:100px" label-width="100px">
-      <el-form-item label="用户名">
-          <el-input style="width:30%"></el-input>
+    <el-form :model="formData" :rules="rules" style="margin-left:100px" label-width="100px">
+      <el-form-item prop="name" label="用户名">
+          <el-input v-model="formData.name" style="width:30%"></el-input>
       </el-form-item>
-      <el-form-item label="简介">
-          <el-input style="width:30%"></el-input>
+      <el-form-item prop="intro" label="简介">
+          <el-input v-model="formData.intro" style="width:30%"></el-input>
       </el-form-item>
-      <el-form-item label="邮箱">
-          <el-input style="width:30%"></el-input>
+      <el-form-item prop="email" label="邮箱">
+          <el-input v-model="formData.email" style="width:30%"></el-input>
       </el-form-item>
       <el-form-item label="手机号">
-          <el-input disabled style="width:30%"></el-input>
+          <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
       </el-form-item>
       <el-form-item>
           <el-button  type="primary">保存信息</el-button>
@@ -30,7 +30,47 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 定义一个表单数据对象
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: ''// 手机号
+      },
+      //   定义一个规则
+      rules: {
+        name: [{ required: true, message: '用户名不能为空' }, {
+          min: 1,
+          max: 7,
+          message: '用户名的长度限制为1到7个字符'
+        }],
+        email: [{ required: true, message: '邮箱不能为空' },
+          {
+            pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+            message: '邮箱格式不正确'
+          }]
+      },
+      //   定义一个默认头像
+      defaultImg: require('../../assets/img/header-logo.jpg')
+    }
+  },
+  methods: {
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo() // 获取用户信息
+  }
+}
 </script>
 
 <style lang="less" scoped>
